42fa91aa3498969312ce00ed39dacdc3333f8614
Inyourdreams <romanchenkopv@gmail.com>
2019-06-03 12:26:18 +0300

    deleting previuos tests

--- src/__tests__/LoginForm.test.js
+++
@@ -1,8 +0,0 @@
-import LoginForm from '../components/LoginForm'
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-it("renders without any errors", () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<LoginForm />, div);
-})
